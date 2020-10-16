40ecc55ffd6e6bed9a7a93411e85fbc26d4fca8d
Pedro Nilo <pedroniloz.nicola@gmail.com>
2020-10-16 20:46:04 -0300

    Finish part 4 of Django Tutorial

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# Django - Write your first Django app
+# Django - Writing your first Django app
 (Tutorial oficial para aprender a utilizar o Framework Django)
 
 Tutorial para criação de site de questionários, visando a assimilação de conceitos e filosofias do Framework, como a manipulação dos dados, área administrativa pré construída, dentre outros.
 
 Como base de dados, optei sair do tradicional SQLite para utilizar o PostgreSQL.
 
-A idéia de refazer esse tutorial, veio, pois mexi com o Django em algumas oportunidades no passado, ainda na versão 1.x, logo para reciclagem e atualização, acredito ser válido.
+A idéia de refazer esse tutorial veio, pois trabalhei com o Django em algumas oportunidades no passado, ainda na versão 1.x, logo para reciclagem e atualização, acredito ser válido.
--- polls_site/polls/views.py
+++ polls_site/polls/views.py
@@ -1,25 +1,41 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponseRedirect
+from django.db.models import F
 from django.shortcuts import get_object_or_404, render
-from .models import Question
+from django.urls import reverse
+from django.views import generic
+from .models import Choice, Question
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-publication_date")[:5]
-    context = dict(latest_question_list=latest_question_list)
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
 
-    return render(request, "polls/index.html", context)
-
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-
-    return render(request, "polls/detail.html", dict(question=question))
+    def get_queryset(self):
+        return Question.objects.order_by("-publication_date")[:5]
 
 
-def results(request, question_id):
-    response = "You're looking at results of question %s."
-    return HttpResponse(response % question_id)
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, "polls/detail.html",
+                      dict(question=question,
+                           error_message="You didn't select a choice."))
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse("polls:results",
+                                    args=(question.id,)))
